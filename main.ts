b45994a28a9d98dadd1b93b26e2ce26cd1c9509e
Gerard Jalón <gerard.jalon@gmail.com>
2025-12-02 23:20:00 +0100

    repte principal

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
 basic.forever(function () {
-	
+    if (input.temperature() < 22) {
+        basic.showString("fred")
+    } else {
+        basic.showString("calor")
+    }
+})
+basic.forever(function () {
+    if (input.soundLevel() > 200) {
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Entertainer), music.PlaybackMode.InBackground)
+    } else if (input.soundLevel() < 50) {
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Prelude), music.PlaybackMode.InBackground)
+    } else {
+        music.stopMelody(MelodyStopOptions.All)
+    }
 })
